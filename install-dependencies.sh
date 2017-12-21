d55ba13324f283107cb30cdff149f84a1514a4b0
Samuel Terra <samuelterra22@hotmail.com>
2017-12-20 23:47:43 -0200

    Exemplos adicionados, dependencias adicionadas.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -5,6 +5,11 @@
 #	http://minerandodados.com.br/index.php/2017/05/19/prevendo-precos-de-acoes-da-bolsa-de-valores-com-machine-learning/
 #	https://www.springboard.com/blog/data-mining-python-tutorial/
 
+echo "The wizard will install the following dependencies:"
+echo "python3-numpy"
+echo ""
+echo ""
+
 echo "Updating repositories..."
 sudo apt-get update
 
@@ -12,19 +17,25 @@ echo "Updating system..."
 sudo apt-get -y dist-upgrade
 
 echo "Installing numpy..."
-sudo apt-get -y install python-numpy
+sudo apt-get -y install python3-numpy
 
 echo "Installing scipy..."
-sudo apt-get -y install python-scipy
+sudo python3.5 -m pip install scipy
 
 echo "Installing pandas..."
-sudo apt-get -y install python-pandas
+sudo apt-get -y install python3-pandas
 
 echo "Installing matplotlib..."
-sudo apt-get -y install python-matplotlib
+sudo apt-get -y install python3-matplotlib
 
 echo "Installing ipython..."
 sudo pip install ipython
 
 echo "Installing scikit-learn..."
 sudo apt-get -y install python-sklearn
+
+echo "Cleaning space..."
+sudo apt-get -y autoreclean
+sudo apt-get -y autoremore
+
+echo "Done."
